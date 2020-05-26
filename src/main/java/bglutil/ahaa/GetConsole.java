2b782f0c1446a92be71d611ee3a511aacfbbb971
Guanglei <toddbgl@hotmail.com>
2020-05-26 08:39:35 +0800

    AllHailAkAs class renamed to GetConsole

--- src/main/java/bglutil/ahaa/GetConsole.java
+++ src/main/java/bglutil/ahaa/GetConsole.java
@@ -20,7 +20,7 @@ import software.amazon.awssdk.services.sts.model.Credentials;
 import software.amazon.awssdk.services.sts.model.GetFederationTokenRequest;
 import software.amazon.awssdk.services.sts.model.GetFederationTokenResponse;
 
-public class AllHailAkAs {
+public class GetConsole {
     public static void main(String args[]) throws Exception{
         // Log setting
         Logger.getRootLogger().setLevel(Level.OFF);
